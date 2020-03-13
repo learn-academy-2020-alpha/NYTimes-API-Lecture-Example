335b33ec94d9ac14817ca8a8ba5facccfc07d460
sjproctor <sproctor950@gmail.com>
2020-03-13 11:12:02 -0700

    adds lecture example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import './App.css'
+import { Jumbotron, Container, InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      search: "",
+      articles: []
+    }
+  }
+
+  getArticles = (query) => {
+    let apiKey = process.env.REACT_APP_NYT_KEY
+    let searchUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${apiKey}`
+
+    fetch(searchUrl)
+    .then((response) => {
+      return response.json()
+    })
+    .then((payload) => {
+      let articles = payload.response.docs
+      this.setState({ articles: articles })
+      console.log(articles)
+    })
+  }
+
+  search = () => {
+    this.getArticles(this.state.search)
+  }
+
+
+  render(){
+    return(
+      <React.Fragment>
+
+      <Jumbotron fluid>
+        <Container fluid>
+          <h1 className="display-3">NY Times Articles</h1>
+          <p className="lead">Search for articles here.</p>
+        </Container>
+      </Jumbotron>
+
+      <InputGroup>
+        <Input
+          placeholder="search articles here"
+          type="text"
+          value={ this.state.search }
+          onChange={ (event) => this.setState({ search: event.target.value })}
+        />
+        <InputGroupAddon addonType="append">
+          <InputGroupText onClick={ this.search }>Search</InputGroupText>
+        </InputGroupAddon>
+      </InputGroup>
+
+      <div>
+        { this.state.articles.map((article, index) => {
+          return(
+            <div key={ index }>
+              <a href={ article.web_url }>
+                { article.abstract }
+              </a>
+            </div>
+          )
+        })}
+      </div>
+
+      </React.Fragment>
+    )
+  }
 }
-
-export default App;
+export default App
